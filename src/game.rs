060d4f996e3c21358b0d10a5cd099c119032ad69
Ryan Angelo <rangelo89@gmail.com>
2024-07-11 22:10:37 -0400

    Working on identifying chains within the board when a new piece is placed.

--- src/game.rs
+++ src/game.rs
@@ -1,12 +1,20 @@
+use std::{borrow::{Borrow, BorrowMut}, collections::{HashMap, LinkedList}};
 
+pub const EMPTY: u8 = 0;
 pub const WHITE: u8 = 1;
 pub const BLACK: u8 = 2;
+pub const WHITE_TERR: u8 = 3;
+pub const BLACK_TERR: u8 = 4;
 
 /**
  *
  * There are traditionaly 181 stones in Go.
  * Traditional Board size is 19x19
  * Also popular are 13x13 and 9x9
+ *
+ * End game scoring: A player's score is the number 
+ * of stones that the player has on the board, plus 
+ * the number of empty intersections surrounded by that player's stones.
  */
 
 /**
@@ -17,51 +25,170 @@ pub const BLACK: u8 = 2;
  */
 pub(crate) struct Board {
     pub board_size: usize,
-    pub board_state: Vec<Vec<u8>>
+    pub board_state: Vec<Vec<Intersection>>,
+    //color -> Identifier String -> Linked List of connected positions
+    pub board_chains: HashMap<String, Vec<(usize, usize)>>,
+    //color -> Identifier String -> Linked List of liberties for connection w/ name String
+    pub board_liberties: HashMap<String, LinkedList<(u8,  u8)>>
+
 }
 
 impl Board {
+
+    pub fn generate_id(row: usize, col: usize) -> String {
+        return row.to_string()+"_"+&col.to_string();
+    }
+
+    /**board_connections
+     * Build a board filled with empty intersections
+     * All Intersections start with their chain being just themselves.
+     */
+    pub fn build_board_start(board_size: usize) -> Vec<Vec<Intersection>> {
+        let mut b_rows = Vec::new();
+        for row in 0..=board_size {
+            let mut b_columns = Vec::new();
+            for column in 0..=board_size {
+                let next_intersection: Intersection = Intersection::new(row, column);
+                b_columns.push(next_intersection);
+            }
+            b_rows.push(b_columns);
+        }
+        return b_rows;
+    }
+
+    pub fn build_chains_start(board_size: usize) -> HashMap<String, Vec<(usize, usize)>> {
+        let mut board_chains: HashMap<String, Vec<(usize, usize)>> = HashMap::new();
+        for row in 0..=board_size {
+            for column in 0..=board_size {
+                //Each new intersection is chained only with itself
+                let chain_vector: Vec<(usize, usize)> = vec![(row, column)];
+                board_chains.insert(Self::generate_id(row, column), chain_vector);
+            }
+        }
+        return board_chains;
+    }
+
     pub fn new(board_size: usize) -> Self {
         Board {
             board_size: board_size,
-            board_state: vec![vec![0; board_size]; board_size],
+            board_state: Self::build_board_start(board_size),
+            board_chains: Self::build_chains_start(board_size),
+            board_liberties: HashMap::new(),
         }
     }
 
-    pub fn place_stone(&mut self, player: &Player, row: usize, col: usize) -> bool {
-        if self.board_state[row][col] == 0 {
-            self.board_state[row][col] = player.player_color;
+    pub fn get_chains() {
+
+    }
+
+    pub fn place_stone(
+    &mut self, player: &Player, row: usize, col: usize) -> bool {
+        if self.board_state[row][col].player_color == 0 {
+            self.board_state[row][col].player_color = player.player_color;
             return true;
         } else {
             return false;
         }
     }
 
+    //Find surrounding adjacent neighbors of the same color
+    pub fn get_friends(&self, intersection: Intersection) -> LinkedList<&Intersection> {
+        //Check left, right, above and below for "friendly" intersections
+        let mut friends: LinkedList<&Intersection> = LinkedList::new();
+    
+        //Check left friend
+        if intersection.col != 0 {
+            if self.board_state[intersection.row][intersection.col-1].player_color == intersection.player_color {
+                friends.push_back(&self.board_state[intersection.row][intersection.col-1]);
+            }
+        }
+
+        //Check right friend
+        if intersection.col+1 != self.board_size {
+            if self.board_state[intersection.row][intersection.col+1].player_color == intersection.player_color {
+                friends.push_back(&self.board_state[intersection.row][intersection.col+1]);
+            }
+        }
+
+        //Check friend above
+        if intersection.row != 0 {
+            if self.board_state[intersection.row+1][intersection.col].player_color == intersection.player_color {
+                friends.push_back(&self.board_state[intersection.row+1][intersection.col]);
+            }
+        }
+
+        //Check friend below
+        if intersection.row-1 != self.board_size {
+            if self.board_state[intersection.row-1][intersection.col].player_color == intersection.player_color {
+                friends.push_back(&self.board_state[intersection.row-1][intersection.col]);
+            }
+        }
+        return friends;   
+    }
+
+    //Call after placing a stone
+    //Check for surrounding area to identify if its been added to a chain
+    pub fn check_for_chain(&mut self, intersection: Intersection) {
+        
+        let friends = self.get_friends(intersection);
+        //All of these friends needs to be made into a single chain
+        //They will now have the id of the newly placed stone
+        for friend in friends {
+            if let Some(links) = self.board_chains.get(&friend.chain_id) {
+                for link in links {
+                    //self.board_state[link.0][link.1].chain_id = intersection.chain_id.clone();
+                }
+            }
+        }
+
+        //Find all intersections that are liberties and update them accordingly
+    }
+
     //Check the last placed location and see if it has created a capture
     //Returns the number of stones captured
     pub fn check_for_capture(&mut self, row: usize, col:usize) -> u8 {
-        
+
         return 0;
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct Intersection {
+    player_color: u8,
+    chain_id: String,
+    row: usize,
+    col: usize,
+}
+
+impl Intersection {
+    pub fn new(row: usize, col: usize) -> Self { 
+        Intersection {
+            player_color: EMPTY, //a new intersection is always an empty intersection
+            chain_id: Board::generate_id(row, col),
+            row: row,
+            col: col,
+        }
     }
 }
 
 pub(crate) struct Player {
     player_color: u8,
-    stones_taken: u8,
+    prisoners: u8,
 }
 
 impl Player {
     pub fn new(new_player_color: u8) -> Self {
         Player {
             player_color: new_player_color,
-            stones_taken: 0,
+            prisoners: 0,
         }
     }
 
     //Add stones that have been taken from opponent
-    pub fn add_stone_taken(&mut self, num_taken_stones: u8) -> u8 {
-        self.stones_taken = self.stones_taken + num_taken_stones;
-        return self.stones_taken;
+    //Only used for territory scoring and not area scoring
+    pub fn add_prisoner(&mut self, num_taken_stones: u8) -> u8 {
+        self.prisoners = self.prisoners + num_taken_stones;
+        return self.prisoners;
     }
 }
 
@@ -75,10 +202,10 @@ mod tests {
         let test_player: Player = Player::new(crate::game::BLACK);
         let mut test_board: Board = Board::new(9);
         let result1 = test_board.place_stone(&test_player, 2, 2);
-        assert_eq!(test_board.board_state[2][2], crate::game::BLACK);
+        assert_eq!(test_board.board_state[2][2].player_color, crate::game::BLACK);
         let result2 = test_board.place_stone(&test_player, 8, 8);
         let result3 = test_board.place_stone(&test_player, 8, 8);
-        assert_eq!(test_board.board_state[8][8], crate::game::BLACK);
+        assert_eq!(test_board.board_state[8][8].player_color, crate::game::BLACK);
         assert_eq!(result1, true);
         assert_eq!(result2, true);
         assert_eq!(result3, false);
@@ -87,28 +214,30 @@ mod tests {
     #[test]
     fn test_add_stone_taken() {
         let mut test_player: Player = Player::new(crate::game::WHITE);
-        test_player.add_stone_taken(5);
-        assert_eq!(test_player.stones_taken, 5);
-        test_player.add_stone_taken(3);
-        assert_eq!(test_player.stones_taken, 8);
+        test_player.add_prisoner(5);
+        assert_eq!(test_player.prisoners, 5);
+        test_player.add_prisoner(3);
+        assert_eq!(test_player.prisoners, 8);
     }
 
     #[test]
     fn test_check_for_capture() {
-        let mut test_player: Player = Player::new(crate::game::WHITE);
+        let mut test_white: Player = Player::new(crate::game::WHITE);
+        let test_black: Player = Player::new(crate::game::BLACK);
         let mut test_board: Board = Board::new(3);
-        test_board.board_state[0][0] = 1;
-        test_board.board_state[0][1] = 1;
-        test_board.board_state[0][2] = 1;
-        test_board.board_state[1][0] = 1;
-        test_board.board_state[1][1] = 2;
-        test_board.board_state[1][2] = 1;
+        test_board.place_stone(&test_white, 0, 0);
+        test_board.place_stone(&test_white, 0, 1);
+        test_board.place_stone(&test_white, 0, 2);
+        test_board.place_stone(&test_white, 0, 3);
+        test_board.place_stone(&test_white, 1, 0);
+        test_board.place_stone(&test_black, 1, 1);
+        test_board.place_stone(&test_black, 1, 2);
         //test_board[2][1] getting acquired by white (1) will capture the black (2) at [1][1]
-        test_board.place_stone(&test_player, 2, 1);
+        test_board.place_stone(&test_white, 2, 1);
         let captured: u8 = test_board.check_for_capture(2, 1); //Should return 1.
         assert_eq!(captured, 1);
-        test_player.add_stone_taken(captured);
-        assert_eq!(test_player.stones_taken, 1);
+        test_white.add_prisoner(captured);
+        assert_eq!(test_white.prisoners, 1);
     }
 
 }
