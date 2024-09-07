9ccd5f1e372a1f4c002e073b9b7a7d56ae60a226
Ryan Angelo <rangelo89@gmail.com>
2024-09-07 07:56:05 -0400

    Added some preliminary grid creation logic based on the css example from bevy. Working on asset management and being able to load an image on rect child of the grid.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod game;
 
+use bevy::color::palettes::css::*;
 use game::Board;
 use game::PlayerModel;
 use bevy::prelude::*;
@@ -9,7 +10,7 @@ impl Plugin for PlayerActionPlugin {
     fn build(&self, app: &mut App) {
         //Add systems here
         app.add_systems(Startup, create_gameboard);
-        app.add_systems(Startup, setup_system);
+        app.add_systems(Startup, spawn_layout);
 
     }
 }
@@ -21,73 +22,177 @@ fn create_gameboard(mut commands: Commands) {
     commands.spawn(player_white_model);
     commands.spawn(player_black_model);
     commands.spawn(game_board);
-    
 }
 
-
-fn setup_system(mut commands: Commands) {
-    // Add a 2D camera
+fn spawn_layout(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
-    // Create a UI root node with Flex direction and spacing
+    let rows = 9;
+    let cols = 9;
+
+    // Top-level grid (app frame)
     commands
         .spawn(NodeBundle {
             style: Style {
-                flex_direction: FlexDirection::Column,
-                align_items: AlignItems::Center,
-                justify_content: JustifyContent::Center,
-                ..Default::default()
+                display: Display::Grid,
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                grid_template_columns: vec![GridTrack::min_content(), GridTrack::flex(1.0)],
+                grid_template_rows: vec![
+                    GridTrack::auto(),
+                    GridTrack::flex(1.0),
+                    GridTrack::px(20.),
+                ],
+                ..default()
             },
-            background_color: Color::NONE.into(),
-            ..Default::default()
+            background_color: BackgroundColor(Color::WHITE),
+            ..default()
         })
-        .with_children(|parent| {
-            // Define the grid dimensions
-            let rows = 3;
-            let cols = 3;
+        .with_children(|builder| {
+            builder
+                .spawn(NodeBundle {
+                    style: Style {
+                        display: Display::Grid,
+                        grid_column: GridPlacement::span(2),
+                        padding: UiRect::all(Val::Px(6.0)),
+                        ..default()
+                    },
+                    ..default()
+                })
+                .with_children(|builder| {
+                    spawn_nested_text_bundle(builder, Default::default(), "The Game of Go");
+                });
 
-            // Create rows
-            for _ in 0..rows {
-                parent
-                    .spawn(NodeBundle {
-                        style: Style {
-                            flex_direction: FlexDirection::Row,
-                            justify_content: JustifyContent::Center,
-                            align_items: AlignItems::Center,
-                            ..Default::default()
-                        },
-                        background_color: Color::NONE.into(),
-                        ..Default::default()
-                    })
-                    .with_children(|row| {
-                        // Create buttons for each row
-                        for _ in 0..cols {
-                            row.spawn(ButtonBundle {
-                                style: Style {
-                                    margin: UiRect::all(Val::Px(5.0)), // Spacing between buttons
-                                    ..Default::default()
-                                },
-                                background_color: Color::rgb(0.25, 0.25, 0.75).into(), // Button color
-                                ..Default::default()
-                            })
-                            .with_children(|button| {
-                                button.spawn(TextBundle {
-                                    text: Text::from_section(
-                                        "Button",
-                                        TextStyle {
-                                            font: Default::default(), // Use Bevy's built-in default font
-                                            font_size: 20.0,
-                                            color: Color::WHITE,
-                                        },
-                                    ),
-                                    ..Default::default()
-                                });
-                            });
+            builder
+                .spawn(NodeBundle {
+                    style: Style {
+                        height: Val::Percent(100.0),
+                        aspect_ratio: Some(1.0),
+                        display: Display::Grid,
+                        padding: UiRect::all(Val::Px(24.0)),
+                        grid_template_columns: RepeatedGridTrack::flex(cols, 1.0),
+                        grid_template_rows: RepeatedGridTrack::flex(rows, 1.0),
+                        row_gap: Val::Px(3.0),
+                        column_gap: Val::Px(3.0),
+                        ..default()
+                    },
+                    background_color: BackgroundColor(Color::srgb(0.25, 0.25, 0.25)),
+                    ..default()
+                })
+                .with_children(|builder| {
+                    // Grid items that are not given an explicit position will be automatically positioned 
+                    // into the next available grid cell. The order in which this is performed can be controlled using the grid_auto_flow
+                    // style property.
+                    for col in 0..cols {
+                        for row in 0..rows {
+                            item_rect(builder, LIGHT_GREY);
                         }
-                    });
-            }
+                    }
+                });
+            // Right side bar (auto placed in row 2, column 2)
+            builder
+                .spawn(NodeBundle {
+                    style: Style {
+                        display: Display::Grid,
+                        align_items: AlignItems::Start,
+                        justify_items: JustifyItems::Center,
+                        padding: UiRect::all(Val::Px(10.)),
+                        // Add an fr track to take up all the available space at the bottom of the column so that the text nodes
+                        // can be top-aligned. Normally you'd use flexbox for this, but this is the CSS Grid example so we're using grid.
+                        grid_template_rows: vec![GridTrack::auto(), GridTrack::auto(), GridTrack::fr(1.0)],
+                        // Add a 10px gap between rows
+                        row_gap: Val::Px(10.),
+                        ..default()
+                    },
+                    background_color: BackgroundColor(BLACK.into()),
+                    ..default()
+                })
+                .with_children(|builder| {
+                    builder.spawn(TextBundle::from_section(
+                        "Sidebar",
+                        TextStyle {
+                            font: Default::default(),
+                            font_size: 24.0,
+                            ..default()
+                        },
+                    ));
+                    builder.spawn(TextBundle::from_section(
+                        "A paragraph of text which ought to wrap nicely. A paragraph of text which ought to wrap nicely. A paragraph of text which ought to wrap nicely. A paragraph of text which ought to wrap nicely. A paragraph of text which ought to wrap nicely. A paragraph of text which ought to wrap nicely. A paragraph of text which ought to wrap nicely.",
+                        TextStyle {
+                            font: Default::default(),
+                            font_size: 16.0,
+                            ..default()
+                        },
+                    ));
+                    builder.spawn(NodeBundle::default());
+                });
+
+            // Footer / status bar
+            builder.spawn(NodeBundle {
+                style: Style {
+                    // Make this node span two grid column so that it takes up the entire bottom row
+                    grid_column: GridPlacement::span(2),
+                    ..default()
+                },
+                background_color: BackgroundColor(WHITE.into()),
+                ..default()
+            });
+
+            // Modal (absolutely positioned on top of content - currently hidden: to view it, change its visibility)
+            builder.spawn(NodeBundle {
+                visibility: Visibility::Hidden,
+                style: Style {
+                    position_type: PositionType::Absolute,
+                    margin: UiRect {
+                        top: Val::Px(100.),
+                        bottom: Val::Auto,
+                        left: Val::Auto,
+                        right: Val::Auto,
+                    },
+                    width: Val::Percent(60.),
+                    height: Val::Px(300.),
+                    max_width: Val::Px(600.),
+                    ..default()
+                },
+                background_color: BackgroundColor(Color::WHITE.with_alpha(0.8)),
+                ..default()
+            });
         });
-    }
+}
+
+/// Create a coloured rectangle node. The node has size as it is assumed that it will be
+/// spawned as a child of a Grid container with `AlignItems::Stretch` and `JustifyItems::Stretch`
+/// which will allow it to take its size from the size of the grid area it occupies.
+fn item_rect(builder: &mut ChildBuilder, color: Srgba) {
+    builder
+        .spawn(NodeBundle {
+            style: Style {
+                display: Display::Grid,
+                padding: UiRect::all(Val::Px(3.0)),
+                ..default()
+            },
+            background_color: BackgroundColor(BLACK.into()),
+            ..default()
+        })
+        .with_children(|builder| {
+            builder.spawn(ImageBundle {
+                background_color: BackgroundColor(color.into()),
+                image: UiImage::default(), // Start with an empty UiImage
+                ..default()
+            });
+        });
+}
+
+fn spawn_nested_text_bundle(builder: &mut ChildBuilder, font: Handle<Font>, text: &str) {
+    builder.spawn(TextBundle::from_section(
+        text,
+        TextStyle {
+            font,
+            font_size: 24.0,
+            color: Color::BLACK,
+        },
+    ));
+}   
 
 /**
  *
@@ -105,5 +210,4 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(PlayerActionPlugin)
         .run();
-
 }
